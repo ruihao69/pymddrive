e2787e48e92a50a9e0dc3ed1a6459da17af121f3
Rui-Hao Bi <biruihao@westlake.edu.cn>
2024-02-20 17:45:14 +0800

    extend setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
 import setuptools
+
+if os.path.exists('requirements.txt'):
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
 
 setuptools.setup(
     name="pymddrive",
@@ -7,5 +12,12 @@ setuptools.setup(
     author_email="biruihao@westlake.edu.cn",
     description="A convenient personal package for molecular dynamics simulation.",
     long_description="A convenient personal package for molecular dynamics simulation.",
+    packages=['pymddrive'],
+    entry_points={
+        'console_scripts': [
+            'simulation_scripts = pymddrive.simulation_scripts:main',
+        ]
+    },
+    requires=requirements,
 )
 
