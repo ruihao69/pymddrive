Fix a fatal issue on tully one pulse three! The gradient was wrong.

pymddrive/models/tully.py
@@ -127,6 +127,7 @@ class TullyOne(NonadiabaticHamiltonian):
 if __name__ == "__main__":
     
     # Testing scalar input
+    t = t0 = 0.0
     x = np.linspace(-10, 10, 1000)
     tl1 = TullyOne()    
     Eg_out = np.zeros_like(x)
@@ -135,7 +136,7 @@ if __name__ == "__main__":
     Fe_out = np.zeros_like(x)
     d12_out = np.zeros_like(x)
     for i, xx in enumerate(x):
-        H, evals, evecs, d12, F = tl1(xx, use_numerical_eigen=True)
+        H, evals, evecs, d12, F = tl1(t, xx, use_numerical_eigen=True)
         Eg_out[i] = evals[0]
         Ee_out[i] = evals[1]
         d12_out[i] = d12[0, 1]
@@ -143,7 +144,9 @@ if __name__ == "__main__":
         Fe_out[i] = F[1]
         
     import matplotlib.pyplot as plt
-    fig = plt.figure(dpi=200, figsize=(3, 2*2))
+    import scienceplots
+    plt.style.use('science')
+    fig = plt.figure(dpi=300, figsize=(3.5, 2.12*2))
     ax_E = fig.add_subplot(211)
     ax_E.plot(x, Eg_out, label="E ground")
     ax_E.plot(x, Ee_out, label="E excited")
@@ -177,7 +180,7 @@ if __name__ == "__main__":
     Fe_out = np.zeros(x.shape[0])
     d12_out = np.zeros(x.shape[0])
     for i, xx in enumerate(x):
-        H, evals, evecs, d12, F = tl1(xx, use_numerical_eigen=True)
+        H, evals, evecs, d12, F = tl1(t, xx, use_numerical_eigen=True)
         Eg_out[i] = evals[0]
         Ee_out[i] = evals[1]
         d12_out[i] = d12[0, 0, 1]
@@ -221,7 +224,7 @@ if __name__ == "__main__":
         d12_out = np.zeros(x.shape[0])
         start = time.time() 
         for i, xx in enumerate(x):
-            H, evals, evecs, d12, F = tl1(xx, use_numerical_eigen=True)
+            H, evals, evecs, d12, F = tl1(t, xx, use_numerical_eigen=True)
             # Eg_out[i] = evals[0]
             # Ee_out[i] = evals[1]
             # d12_out[i] = d12[0, 0, 1]

pymddrive/models/tully_w_pulse.py
@@ -129,7 +129,7 @@ class TullyOnePulseThree(TullyOne):
             
     def get_dHdr(self, t: float, r: Union[float, ArrayLike]):
         dV11 = self.A * self.B * np.exp(-np.abs(r) * self.B)
-        dV12 = -2 * self.C * self.D * r * np.exp(-self.D * r**2) * self.pulse(t)
+        dV12 = -2 * self.C * self.D * r * np.exp(-self.D * r**2)
         return TullyOne._dHdr(dV11, dV12)
             
 # %% Temprarory test code

pymddrive/pulses/morlet.py
@@ -85,6 +85,8 @@ class MorletReal(Pulse):
 if __name__ == "__main__":
     import numpy as np
     import matplotlib.pyplot as plt
+    import scienceplots
+    plt.style.use('science')
     from pymddrive.pulses.pulses import MultiPulse
     # Test for single pulse 
     p1 = MorletReal(A=1, t0=4, tau=1, Omega=10, phi=0)
@@ -95,7 +97,7 @@ if __name__ == "__main__":
     # p = MultiPulse(p1, p2, p3, p4)
     p = p1
     
-    t = np.linspace(-0, 15, 3000)
+    t = np.linspace(-0, 12, 3000)
     sig = [p(tt) for tt in t]
     fig = plt.figure(figsize=(3, 2), dpi=200)
     