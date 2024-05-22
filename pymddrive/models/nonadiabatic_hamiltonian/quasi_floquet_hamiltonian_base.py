0676cbedb45f4a6d171862260aaa2e67c8a7e1c1
Rui-Hao Bi <biruihao@westlake.edu.cn>
2024-05-22 08:39:14 +0800

    add phase to see what happends

--- pymddrive/models/nonadiabatic_hamiltonian/quasi_floquet_hamiltonian_base.py
+++ pymddrive/models/nonadiabatic_hamiltonian/quasi_floquet_hamiltonian_base.py
@@ -21,7 +21,7 @@ class QuasiFloquetHamiltonianBase(HamiltonianBase):
     floquet_type: FloquetType = field(default=None, on_setattr=attr.setters.frozen)
     envelope_function_type: ValidEnvolpeFunctions= field(default=None, on_setattr=attr.setters.frozen)
     NF: int = field(default=None, on_setattr=attr.setters.frozen)
-    
+
     def __init__(
         self,
         dim: int,
@@ -31,57 +31,57 @@ class QuasiFloquetHamiltonianBase(HamiltonianBase):
     ) -> None:
         """ Quasi-Floquet Hamiltonian for a time-dependent Hamiltonian """
         """ whose time dependence is definded by a 'Pulse' object. """
-        
+
         super().__init__(dim)
-        
+
         object.__setattr__(self, "driving_Omega", get_carrier_frequency(ultrafast_pulse))
         # assert (self.driving_Omega>0) and (self.driving_Omega is not None), "The carrier frequency must be a positive number."
         assert self.driving_Omega is not None, "The carrier frequency must be a positive number."
-        
+
         object.__setattr__(self, "floquet_type", get_floquet_type(ultrafast_pulse))
-        object.__setattr__(self, "envelope_function_type", get_envelope_function_type(ultrafast_pulse)) 
-        
+        object.__setattr__(self, "envelope_function_type", get_envelope_function_type(ultrafast_pulse))
+
         assert self.envelope_function_type.value == envelope_pulse.__class__.__name__, \
             f"Invalid envelope function type. Expected {self.envelope_function_type.value}, yet got {envelope_pulse.__class__.__name__}"
-        
+
         object.__setattr__(self, "NF", NF)
         object.__setattr__(self, "ultrafast_pulse", ultrafast_pulse)
         object.__setattr__(self, "envelope_pulse", envelope_pulse)
-        
-    @abstractmethod 
+
+    @abstractmethod
     def H0(self, R: RealVector) -> GenericOperator:
         pass
-    
-    @abstractmethod 
+
+    @abstractmethod
     def H1(self, t: float, RealVector) -> GenericOperator:
         pass
-    
+
     @abstractmethod
     def dH0dR(self, R: RealVector) -> GenericVectorOperator:
         pass
-    
-    @abstractmethod 
-    def dH1dR(self, t: float, R: RealVector) -> GenericVectorOperator: 
+
+    @abstractmethod
+    def dH1dR(self, t: float, R: RealVector) -> GenericVectorOperator:
         pass
-        
+
     def H(self, t: float, R: RealVector) -> GenericOperator:
         H0 = self.H0(R)
-        H1 = self.H1(t, R)
+        H1 = self.H1(t, R) * np.exp(1.j*selfself.driving_Omega*t)
         return self._get_HF(H0, H1, self.driving_Omega, self.NF)
-    
+
     def dHdR(self, t: float, R: RealVector) -> GenericVectorOperator:
         dH0dR = self.dH0dR(R)
         dH1dR = self.dH1dR(t, R)
         return self._get_dHF_dR(dH0dR, dH1dR, self.NF)
-    
+
     def get_floquet_space_dim(self) -> int:
         return get_floquet_space_dim(self.dim, self.NF)
-        
+
     def get_carrier_frequency(self) -> float:
         return self.driving_Omega
-    
+
     def _get_HF(self, H0: GenericOperator, H1: GenericOperator, Omega: float, NF: int) -> GenericOperator:
         return get_HF(self.floquet_type, H0, H1, Omega, NF)
-    
+
     def _get_dHF_dR(self, dH0dR: GenericVectorOperator, dH1dR: GenericVectorOperator, NF: int) -> GenericVectorOperator:
         return get_dHF_dR(self.floquet_type, dH0dR, dH1dR, NF)
